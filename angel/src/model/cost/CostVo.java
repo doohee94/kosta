d7d95fedf6b088cf0eab2adda442fc211ce1a1c9
hyorang <hyorang@DESKTOP-K3MDO92>
2017-05-11 10:18:53 +0900

    vo 만듬

---
+++ angel/src/model/cost/CostVo.java
@@ -0,0 +1,64 @@
+package model.cost;
+
+public class CostVo {
+	//member field
+	private int costNum;
+	private String memberId;
+	private String costDate;
+	private String costContent;
+	private String costCategory;
+	private int costMoney;
+
+	//constructor method 
+	public CostVo()
+	{
+		
+	}
+	public CostVo(int costNum, String memberId, String costDate, String costContent, String costCategory, int costMoney)
+	{
+		this.costNum 		 = costNum;
+		this.memberId		 = memberId;
+		this.costDate		 = costDate;
+		this.costContent 	 = costContent;
+		this.costCategory	 = costCategory;
+		this.costMoney	 = costMoney;
+	}
+	//setter & getter
+	public int getCostNum() {
+		return costNum;
+	}
+	public void setCostNum(int costNum) {
+		this.costNum = costNum;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getCostDate() {
+		return costDate;
+	}
+	public void setCostDate(String costDate) {
+		this.costDate = costDate;
+	}
+	public String getCostContent() {
+		return costContent;
+	}
+	public void setCostContent(String costContent) {
+		this.costContent = costContent;
+	}
+	public String getCostCategory() {
+		return costCategory;
+	}
+	public void setCostCategory(String costCategory) {
+		this.costCategory = costCategory;
+	}
+	public int getCostMoney() {
+		return costMoney;
+	}
+	public void setCostMoney(int costMoney) {
+		this.costMoney = costMoney;
+	}
+	
+}
