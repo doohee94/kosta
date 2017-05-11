8344f76e29b5bbde6b5e4f03e3c7b52f033829e6
hyorang <hyorang@DESKTOP-K3MDO92>
2017-05-11 14:37:35 +0900

    에러 페이지 생성 및 컨트롤 수정-효랭-

--- angel/src/control/cost/CostControl.java
+++ angel/src/control/cost/CostControl.java
@@ -1,11 +1,17 @@
 package control.cost;
 
 import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import command.basic.Command;
+import command.basic.CommandException;
 
 /**
  * Servlet implementation class CostControl
@@ -13,7 +19,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CostControl")
 public class CostControl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private HashMap commandMap;   
+	private String	jspDir = "/cost/";
+	private String  error = "/main/error.jsp";
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -37,5 +45,40 @@ public class CostControl extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+
+		String nextPage = "";
+		String cmdKey	= request.getParameter("cmd");
+		//cmdKey ="list-page"
+		if( cmdKey == null ){
+			cmdKey = "main-page";
+		}
+
+		
+		Command cmd = null;
+
+		try{
+			//commandMap에서 cmdKey값 비교후 명령 실행
+			if( commandMap.containsKey( cmdKey) ){ //.toLowerCase() 			//Hash맵의 있는지 
+				cmd = (Command)commandMap.get( cmdKey); //.toLowerCase() //벨류값 가져오기
+//				cmd = new CommandList("listMessage.jsp")
+			}else{
+				throw new CommandException("지정할 명령어가 존재하지 않음");
+			}
+
+			nextPage = cmd.execute( request ); //명령실행
+//          nextPage =listMessage.jsp
+		}catch( CommandException e ){
+			request.setAttribute("javax.servlet.jsp.jspException", e );
+			nextPage = error;
+			System.out.println("오류 : " + e.getMessage() );
+		}
+		//페이지 이동
+		RequestDispatcher reqDp = getServletContext().getRequestDispatcher( jspDir + nextPage );
+		reqDp.forward( request, response );
+		
+	}
+
 
 }
