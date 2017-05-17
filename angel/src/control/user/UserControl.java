0ca2835cc65beb1ce931766df73dad506e66a607
kosta <kosta@kosta-PC>
2017-05-17 13:50:35 +0900

    UserControl

--- angel/src/control/user/UserControl.java
+++ angel/src/control/user/UserControl.java
@@ -45,6 +45,10 @@ public class UserControl extends HttpServlet {
 		commandMap.put("CoupleMake", new CommandNull("/main/couple_make.jsp"));
 		commandMap.put("Couple_idCheck", new CommandCoupleIdCheck("/main/couple_make.jsp"));
 		commandMap.put("make_couple", new CommandMakeCouple("/plan/plan_person/home.jsp"));
+		
+		commandMap.put("ModifyMember", new CommandModifyView("/main/ModifyView.jsp"));
+		commandMap.put("ModifyMember", new CommandModify("/main/ModifyView.jsp"));
+		
 	}
 
 
