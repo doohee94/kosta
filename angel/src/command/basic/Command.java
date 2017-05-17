0ab683dd69eb77264289cd90bb5dd3f54635e532
kosta <kosta@kosta-PC>
2017-05-17 10:17:00 +0900

    Command

--- angel/src/command/basic/Command.java
+++ angel/src/command/basic/Command.java
@@ -4,5 +4,5 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Command {
-	public String execute( HttpServletRequest request , HttpServletResponse response );
+	public String execute( HttpServletRequest request , HttpServletResponse response ) throws CommandException;
 }
